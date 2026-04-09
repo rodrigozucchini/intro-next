d80968c1ff026bffc20ea4fcef57d2e35e999304
rodrigozucchini <rodrigozucchini1997@gmail.com>
2026-04-09 14:09:23 +0000

    add pokemon + id

--- 02-dashboard/app/dashboard/pokemons/page.tsx
+++ 02-dashboard/app/dashboard/pokemons/page.tsx
@@ -10,6 +10,7 @@ const getPokemons = async( limit= 20, offset= 0 ):Promise<SimplePokemon[]> => {
       id: pokemon.url.split('/').at(-2)!,
       name: pokemon.name
     }))
+
     return pokemons;
 }
 
