9d68e89fd954a77b5c8e962a162f8e595cada85c
rodrigozucchini <rodrigozucchini1997@gmail.com>
2026-04-09 22:42:09 +0000

    add new feature

--- 02-dashboard/app/dashboard/pokemons/index.ts
+++ 02-dashboard/app/dashboard/pokemons/index.ts
@@ -1,3 +1,4 @@
 export type {PokemonsResponse} from './interfaces/pokemons-response';
 export type {SimplePokemon} from './interfaces/simple-pokemons';
-export { PokemonGrid } from './interfaces/components/PokemonGrid'
+export { PokemonGrid } from './interfaces/components/PokemonGrid';
+export type { Pokemon }  from './interfaces/pokemon';
